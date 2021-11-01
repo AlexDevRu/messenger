3e54d20177f39c731741524dfce814bddadaed39
Alexander <a.kulakov.vironit@vironit.ru>
2021-11-01 15:33:23 +0300

    feat: turn notifications on

--- app/src/main/java/com/example/chat/ChatApplication.kt
+++ app/src/main/java/com/example/chat/ChatApplication.kt
@@ -6,7 +6,9 @@ import com.example.chat.di.useCaseModule
 import com.example.chat.di.viewModelModule
 import io.getstream.chat.android.client.ChatClient
 import io.getstream.chat.android.client.logger.ChatLogLevel
+import io.getstream.chat.android.client.notifications.handler.NotificationConfig
 import io.getstream.chat.android.offline.ChatDomain
+import io.getstream.chat.android.pushprovider.firebase.FirebasePushDeviceGenerator
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
@@ -15,8 +17,13 @@ class ChatApplication: Application() {
     override fun onCreate() {
         super.onCreate()
 
+        val notificationConfig = NotificationConfig(
+            pushDeviceGenerators = listOf(FirebasePushDeviceGenerator())
+        )
+
         val client = ChatClient.Builder(getString(R.string.api_key), this)
             .logLevel(ChatLogLevel.ALL)
+            .notifications(notificationConfig)
             .build()
 
         // Step 2 - Set up the domain for offline storage
