376ccf4d579936f67ea655199e0487bba9dd1a35
Alexander <a.kulakov.vironit@vironit.ru>
2021-11-04 14:53:02 +0300

    refactor: move spinner and backhandler composables to another package

--- app/src/main/java/com/example/chat/ui/settings/Settings.kt
+++ app/src/main/java/com/example/chat/ui/settings/Settings.kt
@@ -1,18 +1,20 @@
 package com.example.chat.ui.settings
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Switch
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.chat.R
+import com.example.chat.ui.base.composables.Spinner
 import com.example.chat.ui.base.composables.Toolbar
 import com.example.chat.ui.models.DrawerMenuItem
 import com.example.chat.utils.getActivity
@@ -65,67 +67,5 @@ fun SettingsScreen(
                 }, items = langList, text = state.lang)
             }
         }
-    }
-}
-
-
-@Composable
-fun DropDownList(
-    requestToOpen: Boolean = false,
-    list: List<String>,
-    request: (Boolean) -> Unit,
-    selectedString: (String) -> Unit
-) {
-    DropdownMenu(
-        expanded = requestToOpen,
-        onDismissRequest = { request(false) },
-    ) {
-        list.forEach {
-            DropdownMenuItem(
-                onClick = {
-                    request(false)
-                    selectedString(it)
-                }
-            ) {
-                Text(it)
-            }
-        }
-    }
-}
-@Composable
-fun Spinner(
-    onSelectedChanged: (String) -> Unit,
-    items: List<String>,
-    text: String
-) {
-
-    val isOpen = remember { mutableStateOf(false) }
-    val openCloseOfDropDownList: (Boolean) -> Unit = {
-        isOpen.value = it
-    }
-
-    Box {
-        Column {
-            OutlinedTextField(
-                value = text,
-                readOnly = true,
-                onValueChange = { onSelectedChanged(it) }
-            )
-            DropDownList(
-                requestToOpen = isOpen.value,
-                list = items,
-                openCloseOfDropDownList,
-                onSelectedChanged
-            )
-        }
-        Spacer(
-            modifier = Modifier
-                .matchParentSize()
-                .background(Color.Transparent)
-                .padding(10.dp)
-                .clickable(
-                    onClick = { isOpen.value = true }
-                )
-        )
     }
 }
